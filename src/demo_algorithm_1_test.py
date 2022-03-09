f104a6d22a7cd1f58d4e346388db752c4a83ab0b
James Sutton <1892175+zeryx@users.noreply.github.com>
2022-03-09 05:07:52 -0800

    Adding Algorithmia files

---
+++ src/demo_algorithm_1_test.py
@@ -0,0 +1,7 @@
+from demo_algorithm_1 import apply
+
+def test_algorithm():
+    input = {"matrix_a": [[0, 1], [1, 0]], "matrix_b": [[25, 25], [11, 11]]}
+    result = apply(input)
+    assert result == {"product": [[11., 11.], [25., 25.]]}
+
